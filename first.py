9ef4d48965225ebc9c96c34a949a2a72f21cf655
Muneeb1214-UNSM <muneebmagsiofficial@gmail.com>
2025-05-10 16:14:57 +0500

    add new file

---
+++ first.py
@@ -0,0 +1,9 @@
+light = input("enter light color : ")
+if(light == "red"):
+    print("stop car"):
+elif(light == "yellow"):
+    print("ready car")
+elif(light == "green"):
+    print("go car")
+else:
+    print("your signal is broken")
